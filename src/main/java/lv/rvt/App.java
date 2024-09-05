23906c7bcef850f583fe7a8fc59d0ab9fb9740ee
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-09-05 09:42:25 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,26 @@
 package lv.rvt;
 
+
+import java.util.Scanner;
+
 public class App 
+
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ievadi savu vārdu, uzvārdu un grupu: ");
 
+        String name = scanner.nextLine();
+
+        System.out.println("Sveiks " + name);
+
+       //String name = "Ralfs";
+       //int age = 20;
+       //boolean isOnline = false;
+
+       System.out.println(name);
     }
 
 }
