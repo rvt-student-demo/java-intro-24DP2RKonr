53061d8aff1c29cc9dd8beb22b1e59b925a98333
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-09-11 09:33:36 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,38 +1,29 @@
 package lv.rvt;
 
+import java.util.*;
 
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthStyle;
 
 public class App 
 
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi savu vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvārdu: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ievadi savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: "+ name + " " + lastname + ", " + group);
         
-       // System.out.println("Ievadi savu vārdu, uzvārdu un grupu: ");
-       // String name = scanner.nextLine();
-       // System.out.println("Sveiks " + name);
-       //String name = "Ralfs";
-       //int age = 20;
-       //boolean isOnline = false;
-       //System.out.println(name);
-       
-        System.out.println("Chicken:");
-        String chicken = scanner.nextLine();
-        System.out.println("Bacon(kg)");
-        String bekons = scanner.nextLine();
-        System.out.println("Tractor:");
-        String traktors = scanner.nextLine();
-        System.out.println(chicken);
-
-        System.out.println("And finally, a summary");
-        System.out.println(chicken);
-        System.out.println(bekons);
-        System.out.println(traktors);
-       
-
-
 
     }
 
