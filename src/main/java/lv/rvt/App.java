5af5d8985019fb8c544e9d958368a5994f921e58
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-10-02 08:19:41 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -14,6 +14,9 @@ public class App
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give numbers:");
         int Sum = 0;
+        int Count = 0;
+        int evenCount = 0;
+        int oddCount = 0;
         while (true) {
             int number = scanner.nextInt(); 
             if (number == -1) {  
@@ -21,8 +24,19 @@ public class App
     
             }
             Sum = Sum + number;
+            Count = Count +1;
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
         }
         System.out.println("Thx! Bye!");  
             System.out.println("Sum: " + Sum);
+            System.out.println("Count: " + Count);
+            System.out.println("Average: ");
+            System.out.println("Even: " + evenCount);
+            System.out.println("Odd: " + oddCount);
+
+        }
     }
 }
