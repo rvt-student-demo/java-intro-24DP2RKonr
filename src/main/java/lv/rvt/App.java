e617e82d9476ccb8ba949e3a6469c11e4db32f53
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-10-03 09:20:37 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 import javax.swing.plaf.synth.SynthStyle;
 
 public class App 
-
 {
-    public class Main {
-
-        public static void main(String[] args) {
-            PrintUntilNumber(5);
-        }
-    
-        public static void PrintUntilNumber(int first) {
-            int i = 1;
-            while (i != first + 1) {
-                System.out.println(i);
-                i++;
+            public static void main(String[] args )
+            {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Give a start number:");
+            int num1 = Integer.valueOf(scanner.nextLine());
+            System.out.println("Give a finish number:");
+            int num2 = Integer.valueOf(scanner.nextLine());
+            divisibleByThreeInRange(num1, num2);
             }
-        }
-    }
-    
+            public static void divisibleByThreeInRange(int num1, int num2) {
+                for (int i = num1; i <= num2; i++);{
+                    if (i % 3 == 0) {
+                        System.out.println(i);
+                    }
+                }
             }
+           }
