a0715cb5b2dba4aaa9afa6361f8ad03a0e323a37
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-10-09 09:48:12 +0000

    Star sign (1,2 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,20 +8,9 @@ import javax.swing.plaf.synth.SynthStyle;
 
 public class App 
 {
-            public static void main(String[] args )
-            {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Give a start number:");
-            int num1 = Integer.valueOf(scanner.nextLine());
-            System.out.println("Give a finish number:");
-            int num2 = Integer.valueOf(scanner.nextLine());
-            divisibleByThreeInRange(num1, num2);
-            }
-            public static void divisibleByThreeInRange(int num1, int num2) {
-                for (int i = num1; i <= num2; i++);{
-                    if (i % 3 == 0) {
-                        System.out.println(i);
-                    }
-                }
-            }
-           }
+    public static void main(String[] args ) {
+        Stars.printSquare(4);
+    }
+}
+
+
