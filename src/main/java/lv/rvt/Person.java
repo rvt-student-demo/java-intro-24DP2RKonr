791119af4118d17f63908851760dec5a5397c428
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-10-24 09:36:12 +0000

    Person

---
+++ src/main/java/lv/rvt/Person.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+
+public class Person {
+    private String name;
+    private int age;
+}
+
+    //Class constructor
+    public Person(String name) {
+        this.name = name;
+        this.age = 0;
+    }
