ad933709eed137aec0dd73769d178e63a9430404
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-10-30 10:40:46 +0000

    Film

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -2,36 +2,27 @@ package lv.rvt;
 
 import java.util.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        DecreasingCounter counter1 = new DecreasingCounter(10);
-        System.out.println("Part 1:");
-        counter1.printValue();    
-        counter1.decrement();
-        counter1.printValue();    
-        counter1.decrement();
-        counter1.printValue();    
 
-        DecreasingCounter counter2 = new DecreasingCounter(2);
-        System.out.println("\nPart 2:");
-        counter2.printValue();    
-        counter2.decrement();
-        counter2.printValue();    
-        counter2.decrement();
-        counter2.printValue();    
-        counter2.decrement();
-        counter2.printValue();    
+        Film chipmunks = new Film("Alvin and the Chipmunks: The Squeakquel", 0);
 
-        DecreasingCounter counter3 = new DecreasingCounter(100);
-        System.out.println("\nPart 3:");
-        counter3.printValue();    
-        counter3.reset();
-        counter3.printValue();    
-        counter3.decrement();
-        counter3.printValue();    
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(reader.nextLine());
+
+        System.out.println();
+        if (age >= chipmunks.ageRating()) {
+            System.out.println("You may watch the film " + chipmunks.name());
+        } else {
+            System.out.println("You may not watch the film " + chipmunks.name());
+        }
     }
 }
+
 
     
     
