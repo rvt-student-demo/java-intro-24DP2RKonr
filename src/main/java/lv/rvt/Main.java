6b86908bd06a3e3dde7b5a7fcdce31336f7e5650
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-11-07 09:41:12 +0000

    Payment Card (6 parts)

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -5,31 +5,28 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Statistics allNumbers = new Statistics();
-        Statistics evenNumbers = new Statistics();
-        Statistics oddNumbers = new Statistics();
-
-        System.out.println("Enter numbers:");
-        while (true) {
-            int number = scanner.nextInt();
-            if (number == -1) {
-                break;
-            }
-            allNumbers.addNumber(number);
-            if (number % 2 == 0) {
-                evenNumbers.addNumber(number);
-            } else {
-                oddNumbers.addNumber(number);
-            }
+        public static void main(String[] args) {
+            PaymentCard paulsCard = new PaymentCard(20);
+            PaymentCard mattsCard = new PaymentCard(30);
+    
+            paulsCard.eatHeartily();
+            mattsCard.eatAffordably();
+    
+            System.out.println("Paul: " + paulsCard);
+            System.out.println("Matt: " + mattsCard);
+    
+            paulsCard.addMoney(20);
+            mattsCard.eatHeartily();
+    
+            System.out.println("Paul: " + paulsCard);
+            System.out.println("Matt: " + mattsCard);
+    
+            paulsCard.eatAffordably();
+            paulsCard.eatAffordably();
+            mattsCard.addMoney(50);
+    
+            System.out.println("Paul: " + paulsCard);
+            System.out.println("Matt: " + mattsCard);
         }
-
-        System.out.println("Count: " + allNumbers.getCount());
-        System.out.println("Sum: " + allNumbers.sum());
-        System.out.println("Average: " + allNumbers.average());
-
-        System.out.println("Sum of even numbers: " + evenNumbers.sum());
-        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
     }
-}
+    
