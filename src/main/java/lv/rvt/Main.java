a3799f5e85acec4b7ef1e184faa46918d4ebe65a
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-11-07 08:19:03 +0000

    Statistics (4 parts)

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -6,14 +6,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Agent bond = new Agent("James", "Bond");
-        System.out.println(bond);  // Output: My name is Bond, James Bond
-    
-        Agent ionic = new Agent("Ionic", "Bond");
-        System.out.println(ionic);  // Output: My name is Bond, Ionic Bond
+        Scanner scanner = new Scanner(System.in);
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+        System.out.println("Enter numbers:");
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+                break;
+            }
+            allNumbers.addNumber(number);
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
+        }
+
+        System.out.println("Count: " + allNumbers.getCount());
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Average: " + allNumbers.average());
+
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
     }
-}    
-
-
-    
-    
+}
