55448fec70bfb917b5cedcca3f22589223eccb5c
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-11-28 11:20:55 +0000

    Health station (3 parts), labots

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,11 +8,9 @@ public static void main(String[] args) {
     Person ethan = new Person("Ethan", 1, 110, 7);
     Person peter = new Person("Peter", 33, 176, 85);
 
-    // Test weighing
     System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
     System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
 
-    // Test feeding
     childrensHospital.feed(ethan);
     childrensHospital.feed(ethan);
     childrensHospital.feed(ethan);
@@ -22,7 +20,6 @@ public static void main(String[] args) {
     System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
     System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
 
-    // Test weighings count
     System.out.println("weighings performed: " + childrensHospital.weighings());
     childrensHospital.weigh(ethan);
     childrensHospital.weigh(peter);
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -7,7 +7,6 @@ public class Person {
     private int height;
     private int weight;
 
-    // Constructor
     public Person(String name, int age, int height, int weight) {
         this.name = name;
         this.age = age;
@@ -15,20 +14,16 @@ public class Person {
         this.weight = weight;
     }
 
-    // Getter for name
     public String getName() {
         return this.name;
     }
 
-    // Getter for weight
     public int getWeight() {
         return this.weight;
     }
 
-    // Setter for weight
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    // Other getters can be added if needed
 }
