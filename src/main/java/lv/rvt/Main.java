f5fbd73bf6db67ce2d5eaff16c76e414dfc4d475
23DP2RKonr <166109476+24DP2RKonr@users.noreply.github.com>
2024-12-05 12:38:30 +0000

    Overloaded Counter (2 parts)

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,14 +1,28 @@
 package lv.rvt;
 
 public class Main {
-        public static void main(String[] args) {
-            ConstructorOverload tapeMeasure = new ConstructorOverload("Tape measure");
-            ConstructorOverload plaster = new ConstructorOverload("Plaster", "home improvement section");
-            ConstructorOverload tyre = new ConstructorOverload("Tyre", 5);
-    
-            System.out.println(tapeMeasure);
-            System.out.println(plaster);
-            System.out.println(tyre);
-        }
+    public static void main(String[] args) {
+        Counter counter = new Counter(10);
+        System.out.println(counter.value()); // 10
+
+        counter.increase();
+        System.out.println(counter.value()); // 11
+
+        counter.decrease();
+        System.out.println(counter.value()); // 10
+
+        counter.increase(5);
+        System.out.println(counter.value()); // 15
+
+        counter.decrease(3);
+        System.out.println(counter.value()); // 12
+
+        counter.increase(-2); // No effect
+        System.out.println(counter.value()); // 12
+
+        counter.decrease(-3); // No effect
+        System.out.println(counter.value()); // 12
     }
+}
+
     
